2df9f8777be9a76eb09de56e8547904241dd9282
Hidenori Miyoshi <miyohide@gmail.com>
2013-12-04 12:14:01 +0900

    Add account_id column to Posts table && association

--- db/migrate/003_add_acount_to_post.rb
+++ db/migrate/003_add_acount_to_post.rb
@@ -3,6 +3,9 @@ class AddAcountToPost < ActiveRecord::Migration
     change_table :posts do |t|
       t.integer :account_id
     end
+    # すでに存在しているpostsに対して、userを紐付ける
+    first_account = Account.first
+    Post.all.each { |p| p.update_attribute(:account, first_account) }
   end
 
   def self.down
